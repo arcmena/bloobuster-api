957c351a428e8101fbbf39b2c55ef81578745490
Marcelo Mena <marcelomena010@gmail.com>
2020-06-28 05:36:35 -0300

    Deleted tmp

--- tmp/config/environment.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const { NODE_ENV } = process.env;
-NODE_ENV &&
-    require("dotenv").config({
-        path: `${__dirname}/../../.env.${NODE_ENV}`,
-    });
-//# sourceMappingURL=environment.js.map
